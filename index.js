8065801bd935bc1a2ec4bb6f5f06886489997771
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2022-10-26 14:27:55 +0600

    commit message

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+const courses = require("./data.json");
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((course) => course.id === id);
+  console.log(selectedCourse);
+  res.send(selectedCourse);
+});
+app.get("/checkout/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((course) => course.id === id);
+  console.log(selectedCourse);
+  res.send(selectedCourse);
+});
+app.listen(port, () => {});
